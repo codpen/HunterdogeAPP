154d8b8e686eb30697f8acda0e2bb74670667b11
codpen <davidpeng913@gmail.com>
2021-12-07 20:20:20 +0300

    mobile menu close button

--- src/blocks/menu/newMenu/newMenu.jsx
+++ src/blocks/menu/newMenu/newMenu.jsx
@@ -1,12 +1,12 @@
-import React, {useState} from 'react';
-import {LinkWrapper} from "../../../components/common";
+import React, { useState } from 'react';
+import { LinkWrapper } from "../../../components/common";
 import hunterdogeShadow from '../../../images/hunterdoge_menu.png';
 import ContactModal from "../../../components/modal/Contact";
 import SearchInput from "../../../components/searchInput";
-import {Flex, ImageWrapper, Wrapp, WrappedButton, WrappedLink, Wrapper, CloseButton, Title} from "./MenuStyled";
+import { Flex, ImageWrapper, Wrapp, WrappedButton, WrappedLink, Wrapper, CloseButton, Title } from "./MenuStyled";
 import ConnectWallet from '../../../connection/ConnectWallet';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
-import {Button} from "@mui/material";
+import { Button } from "@mui/material";
 const NewMenu = (props) => {
     const [open, setOpen] = useState(false)
     const mobileMatches = useMediaQuery('(min-width:600px)');
@@ -19,31 +19,31 @@ const NewMenu = (props) => {
             <Wrapper>
                 {!mobileMatches && <Title>HUNTERDOGE</Title>}
                 <Wrapp>
-                    {!mobileMatches && <CloseButton onClick={() => props.setOpen(false)}>X</CloseButton> }
-                    <SearchInput small mb={mobileMatches? '28px': '0px'}/>
+                    {!mobileMatches && <CloseButton onClick={() => props.setOpen(false)}>X</CloseButton>}
+                    <SearchInput small mb={mobileMatches ? '28px' : '0px'} />
                 </Wrapp>
-                {!mobileMatches && 
-                <WrappedButton onClick={onClickRegister} sx={{mt: 1.5}}>
-                    register
-                </WrappedButton>
+                {!mobileMatches &&
+                    <WrappedButton onClick={onClickRegister} sx={{ mt: 1.5 }}>
+                        register
+                    </WrappedButton>
                 }
-                <LinkWrapper to="/allTokens">
+                <LinkWrapper onClick={() => { props.setOpen(false) }} to="/allTokens">
                     <WrappedButton>all tokens</WrappedButton>
                 </LinkWrapper>
-                <LinkWrapper to="/nft-gallery">
+                <LinkWrapper onClick={() => { props.setOpen(false) }} to="/nft-gallery">
                     <WrappedButton to="/nft-gallery">NFT GALLERY</WrappedButton>
                 </LinkWrapper>
-                <a href="https://t.me/huntersground" target="_blank" style={{width: '100%'}}>
+                <a onClick={() => { props.setOpen(false) }} href="https://t.me/huntersground" target="_blank" style={{ width: '100%' }}>
                     <WrappedButton>telegram shill bot</WrappedButton>
                 </a>
-                <WrappedButton onClick={() => setOpen(true)}>contact</WrappedButton>
+                <WrappedButton onClick={() => { props.setOpen(false); setOpen(true) }}>contact</WrappedButton>
                 <Flex>
                     <WrappedLink to={'https://t.me/hunterdogeofficial'}>+ Submit your coin</WrappedLink>
-                    <ImageWrapper src={hunterdogeShadow}/>
+                    <ImageWrapper src={hunterdogeShadow} />
                 </Flex>
-                {open && <ContactModal setIsOpen={setOpen}/>}
+                {open && <ContactModal setIsOpen={setOpen} />}
             </Wrapper>
-            
+
         </>
 
     );
